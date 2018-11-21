73e51f0315f66d8ae6cffe9487e70cb4b294ada3
RAJAT KHEMKA <rkhemka@atrihub.io>
2018-11-20 18:34:42 -0800

    test:(Tdd)
    added unit test case and understood how to fix a test case
    
    ICTRAINING-209

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
@@ -9,13 +10,14 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-
-
-
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
+  #      self.assertTrue(html.strip().endswith('</html>'))
+        # had to use print(repr(html)) to understand new line character added at end of line
+        #<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <title>To-Do lists</title>\n</head>\
+        # n<body>\n\n</body>\n</html>'
+
