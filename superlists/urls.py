182c70f7f5a012afef8e2c068f210d1137e7bec9
RAJAT KHEMKA <rkhemka@atrihub.io>
2018-11-27 11:35:25 -0800

    refractor(TDD):part1 chapter 5
    understood REST mapping and solved issues of 11/26/2018
    
    ICTRAINING-209

--- superlists/urls.py
+++ superlists/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from lists import views
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('admin/', admin.site.urls),
+    path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
 ]
