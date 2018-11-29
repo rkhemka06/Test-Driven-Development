a9dbec84e9a74970ffedcd6646338a51ec4015ae
RAJAT KHEMKA <rkhemka@atrihub.io>
2018-11-29 11:57:23 -0800

    fix:(TDD)
    Updating  database information in Travis CI.yml
    
    ICTRAINING-209

--- superlists/settings.py
+++ superlists/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Tdd',  # mysite_db replaced by rajatkhemka
         'USER': 'rajatkhemka',
-    
+
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
