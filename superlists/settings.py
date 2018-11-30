0a5a520dffcc53ac1c5bdd2914b588ecc41bc202
RAJAT KHEMKA <rkhemka@atrihub.io>
2018-11-30 10:20:05 -0800

    feat:(TDD)
    Deploying on Heroku
    
    ICTRAINING-209

--- superlists/settings.py
+++ superlists/settings.py
@@ -125,6 +125,17 @@ USE_TZ = True
 #     'default': dj_database_url.config(env='TEST_DATABASE_URL')
 # }
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
